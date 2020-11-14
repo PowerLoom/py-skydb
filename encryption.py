aa3e8ee31fd886e3f010fe0d65c98ec9f46e7616
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-11-14 18:07:45 +0530

    Added function to encode Numbers

--- encryption.py
+++ encryption.py
@@ -3,11 +3,36 @@
     official package skynet-js nodejs package, the crypto.ts file
 """
 import nacl
+import hashlib
 from backports.pbkdf2 import pbkdf2_hmac # For generating the a proper 32 byte password from the seed
+
+def blake2b(data_key):
+    """
+    Hash the given data_key using the blake2b algorithm to generate a 32bytes(256bits) hash
+    Args:
+        data_key(str): This is the data_key that will be used to retrieve entries from the registry
+    """
+    pass
+
+def encode_num(num):
+    """
+    This function is based on the encodeNumber function on the crypto.ts file
+    Args:
+        num(int): This is the number that needs to be encoded
+    Returns:
+        encoded_num(list): A list of 8 ints which are derived from the encoded num
+    """
+    _bytes = (num).to_bytes(8, byteorder='little')
+    encoded_num = list(_bytes)
+    return encode_num
+
 
 def genKeyPairFromSeed(seed):
     """
     Generate a Public key, Private Key pair from the seed using Ed25519 Algorithm
+    Args:
+        seed(str): Any random seed can be used. Make sure to remember the seed so that you
+        can re-generate the public and private keys if your ever lose them.
     Returns:
         public_key(bytes), private_key(bytes): Both are public_key and private_keys.
     """
@@ -17,3 +42,5 @@ def genKeyPairFromSeed(seed):
     seed = seed.encode("utf-8")
     key = pbkdf2_hmac("sha256", seed, b"", 1000, 32)
     return nacl.bindings.crypto_sign_seed_keypair(key)
+
+
