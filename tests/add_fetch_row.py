eb07288a42c931e2ef06e32ed8683c4e76a6c513
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-11-18 12:35:04 +0530

    Added new test conditions

--- tests/add_fetch_row.py
+++ tests/add_fetch_row.py
@@ -1,13 +1,12 @@
-from skydb_utils import SkydbTable
+from skydb import SkydbTable
 
-table = SkydbTable(table_name="MyTable", columns=['c1','c2'], seed="RANDOM SEED")
+table = SkydbTable(table_name="asdacxccaf", columns=['c1','c2'], seed="RANDOM SEED")
 
 _ = table.add_row({'c1':'Data 1', 'c2': 'HoHoHo a'})
 _ = table.add_row({'c1':'Data 2', 'c2': 'HoHoHo b'})
 _ = table.add_row({'c1':'Data 3', 'c2': 'HoHoHo c'})
 _ = table.add_row({'c1':'Data 4', 'c2': 'HoHoHo d'})
-
-print(table.fetch_row(0))
-print(table.fetch_row(1))
-print(table.fetch_row(2))
+print('fetching a table')
 print(table.fetch_row(3))
+
+print(table.fetch(condition={'c1':'Data 2'}, start_index=_))
--- tests/test_new_fetch_function.py
+++ tests/test_new_fetch_function.py
@@ -6,40 +6,63 @@
 		- Make sure the fetch function is able to retreive only the given number of rows.
 """
 from skydb import SkydbTable
+from skydb.skydb_utils import _value_in
 from random import choice
 from string import ascii_letters
 
 table_name = ''.join([choice(ascii_letters) for i in range(20)]) # Generate a random table name
 seed = "SEED"
 
-table = SkydbTable(table_name, columns=['c1','c2','c3'], seed=seed)
+table = SkydbTable(table_name, columns=['col1'], column_split=['c1','c2','c3'], seed=seed)
 
-row_index = table.add_row({'c1':'Data-1', 'c2':'Data-2', 'c3': 'Data-3'}) 
-row_index = table.add_row({'c1':'Data-5', 'c2':'Data-6', 'c3': 'Data-6'})
-row_index = table.add_row({'c1':'Data-1', 'c2':'Data-2', 'c3': 'Data-3'})
-row_index = table.add_row({'c1':'Data-5', 'c2':'Data-6', 'c3': 'Data-6'})
+row_index = table.add_row({'col1':'Data-1;Data-2;Data-3'}) 
+row_index = table.add_row({'col1':'Data-5;Data-6;Data-7'})
+row_index = table.add_row({'col1':'Data-1;Data-2;Data-3'})
+row_index = table.add_row({'col1':'Data-5;Data-6;Data-9'})
 
 def test_fetch_2_rows():
 	global table, row_index
 	rows = table.fetch(
-				condition={'c1':'Data-5','c2':'Data-6'},
+				condition={'col1':['c1','Data-5']},
 				start_index=row_index,
-				n_rows=2
+				n_rows=2,
+				condition_func=_value_in
 			)
 	assert len(rows) == 2, "Test case failed. The fetched rows is not the same as n_rows"
+	print("-"*40)
+	print(rows)
 	
 	for k in rows:
-		assert rows[k]['c1'] == 'Data-5', "Test case failed. Invalid Data retreived"
-		assert rows[k]['c2'] == 'Data-6', "Test case failed. Invalid Data retreived"
+		assert rows[k]['col1'].split(';')[0] == 'Data-5', "Test case failed. Invalid Data retreived"
 
-def test_fetch_none():
+def test_fetch_1_rows():
 	global table, row_index
 	rows = table.fetch(
-				condition={'c1':'Data-10','c2':'Data-6'},
+				condition={'col1':['c1','Data-5'], 'col1':['c3','Data-7']},
 				start_index=row_index,
-				n_rows=2
+				n_rows=2,
+				condition_func=_value_in
+			)
+	assert len(rows) == 1, "Test case failed. The fetched rows is not the same as n_rows"
+	print("-"*40)
+	print(rows)
+	
+	for k in rows:
+		assert rows[k]['col1'].split(';')[0] == 'Data-5', "Test case failed. Invalid Data retreived"
+		assert rows[k]['col1'].split(';')[2] == 'Data-7', "Test case failed. Invalid Data retreived"
+
+def test_fetch_0_rows():
+	global table, row_index
+	rows = table.fetch(
+				condition={'col1':['c1','Data-5'], 'col1':['c3','Data-10']},
+				start_index=row_index,
+				n_rows=2,
+				condition_func=_value_in
 			)
 	assert len(rows) == 0, "Test case failed. The fetched rows is not the same as n_rows"
+	print("-"*40)
+	print(rows)
 
 test_fetch_2_rows()
-test_fetch_none()
+test_fetch_1_rows()
+test_fetch_0_rows()
